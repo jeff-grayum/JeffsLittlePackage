ad89e0fa5dc1553c772051e0c2a37abba4b2b61d
Jeff Grayum <35897282+jeff-grayum@users.noreply.github.com>
2023-06-09 17:52:25 -0400

    Add files via upload

---
+++ JeffsLittlePackage.R
@@ -0,0 +1,11 @@
+#Creating bigload function.
+bigload<- function(package){
+  package <- deparse(substitute(package))
+  if (!requireNamespace(package, quietly = TRUE)) {
+    install.packages(package)
+  }
+  if (!require(package, character.only = TRUE)) {
+    stop("Failed to load package: ", package)
+  }
+}
+
